e921e2403397b3637fab9f8ce7e8129a70f56980
Theo OLLIVIER <theo.ollivier@gmail.com>
2013-05-05 09:52:04 +0100

    added validates option for subscribe and booking

--- Restaurant/app/models/booking.rb
+++ Restaurant/app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ActiveRecord::Base
   attr_accessible :bookingDate, :customerName, :peopleNo, :email, :contactNumber
+  validates :peopleNo, :presence => true, :inclusion => { :in => 1..6 }
+  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :update }
+  validates :customerName, :presence => true, :length => { :maximum => 30 }
+  validates :contactNumber, :presence => true, :length =>  { :is => 10 }, :numericality => { :only_integer => true }
   
 end
--- Restaurant/app/models/subscribe.rb
+++ Restaurant/app/models/subscribe.rb
@@ -1,3 +1,7 @@
 class Subscribe < ActiveRecord::Base
   attr_accessible :contactNo, :customerName, :email, :password
+  validates :contactNo, :presence => true, :length =>  { :is => 10 }, :numericality => { :only_integer => true }
+  validates :customerName, :presence => true, :length => { :maximum => 30 }
+  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :update }
 end
